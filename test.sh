110628451dd4b1290e8306c88cc185c741625f30
Khoi Hoang <khoi_hoang@hotmail.com>
2019-08-24 20:56:07 +0900

    Updated test script

--- test.sh
+++ test.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-cc -g $1/$1_test.c -o /tmp/kdb_test &&\
-valgrind --leak-check=full --error-exitcode=1 /tmp/kdb_test
-#rm -f a.out
+set -e
+
+MODULES=(string entry list)
+
+function test_module() {
+	cc -g $1/$1_test.c -o /tmp/kdb_test &&\
+		valgrind --leak-check=full --error-exitcode=1 /tmp/kdb_test
+}
+
+for i in "${MODULES[@]}"
+do
+	printf '=%.0s' {1..80}
+	echo
+	echo ${i^^}
+	printf '=%.0s' {1..80}
+	echo
+
+	test_module $i
+done
