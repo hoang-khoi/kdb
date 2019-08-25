0bb1f56fd2a75eb6b6b36b0ebd2a986c590da9f1
Khoi Hoang <khoi_hoang@hotmail.com>
2019-08-25 15:03:49 +0900

    Test script can be executed on specific modules

--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
 
-MODULES=(string entry list hash string_entry list_string_entry)
+modules="string entry list hash string_entry list_string_entry"
 
 function test_module() {
 	cc -g $1/$1_test.c -o /tmp/kdb_test &&\
 		valgrind --leak-check=full --error-exitcode=1 /tmp/kdb_test
 }
 
-for i in "${MODULES[@]}"
+if [ $# -gt 0 ]; then
+	modules=$@
+fi
+
+for i in $modules
 do
 	printf '=%.0s' {1..80}
 	echo
