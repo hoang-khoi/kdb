a75439244bdef2f5e1c84822e47f65f619b8d6c3
Khoi Hoang <khoi_hoang@hotmail.com>
2019-09-14 20:23:55 +0900

    Beautified string_entry

--- string_entry/string_entry.h
+++ string_entry/string_entry.h
@@ -8,15 +8,23 @@
  * Specific APIs for string key-value entries.
  */
 
+/*
+ * Creates a new key-value entry.
+ */
 struct entry *string_entry_new(const char *key, const char *value);
+/*
+ * Frees a key-value entry.
+ */
 void string_entry_free(void *e);
 /*
- * Returns 1 if key equals to buffer, 0 otherwise.
+ * Returns 1 if the entry's key equals to buffer, 0 otherwise.
  */
 int string_entry_key_equals(const struct entry *e, const char *buffer);
+/*
+ * Returns entry key's hash value.
+ */
 unsigned long string_entry_hash(const struct entry *e,
 				unsigned long (*hash_func)(const char*));
-
 /*
  * Dumps string entry to stdout for debugging.
  */
