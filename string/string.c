98da9257a504bfe86d60cbc5c876f18674d4f4b4
Khoi Hoang <khoi_hoang@hotmail.com>
2019-09-14 11:23:37 +0900

    Beautified string

--- string/string.c
+++ string/string.c
@@ -1,4 +1,5 @@
 #include "string.h"
+
 #include "../formatter/formatter.h"
 
 #include <string.h>
--- string/string.h
+++ string/string.h
@@ -1,26 +1,40 @@
 #ifndef STRING_H
 #define STRING_H
 
+/*
+ * String that is aware of its length.
+ */
+
 struct string {
 	unsigned long length;
 	char *buffer;
 };
 
-struct string *string_new(const char *buffer);
-void string_free(struct string *str);
-
 /*
- * Returns the C-style string.
+ * Creates a new string from buffer.
+ */
+struct string *string_new(const char *buffer);
+/*
+ * Release string's memory.
+ */
+void string_free(struct string *str);
+/*
+ * Returns C-style string buffer.
  */
 char *string_get_buffer(const struct string *str);
+/*
+ * Gets string's length.
+ */
 unsigned long string_length(const struct string *str);
 /*
- * Returns 1 if strings are equal to each other, 0 otherwise.
+ * Returns 1 if strings are equal to a buffer, 0 otherwise.
  */
 int string_equals(const struct string *s, const char *buffer);
+/*
+ * Returns hash value from a hash algorithm.
+ */
 unsigned long string_hash(const struct string *s,
 			  unsigned long (*hash_func)(const char*));
-
 /*
  * Dumps string to stdout for debugging.
  */
