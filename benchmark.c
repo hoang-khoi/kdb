8f3f1a39d2bdaa3717e34814354c57e3295cee52
Khoi Hoang <khoi_hoang@hotmail.com>
2019-09-16 20:48:01 +0900

    Added benchmark for linked-list(?)

--- benchmark.c
+++ benchmark.c
@@ -14,6 +14,20 @@
 #define REPORT_FILE_GET "report_get.txt"
 
 /*
+ * Performs benchmarking adhtable.
+ */
+static void benchmark_adhtable(FILE *fdata, FILE *fset, FILE *fget);
+/*
+ * Perforams benchmarking dhtable.
+ */
+static void benchmark_dhtable(FILE *fdata, FILE *fset, FILE *fget);
+
+/*
+ * Measure how stupid it is to use a list as a "hash table".
+ */
+static void benchmark_list(FILE *fdata, FILE *fset, FILE *fget);
+
+/*
  * Simple wrapper over adhtable_set(), but clock_t will be
  * returned to measure the operation's performance.
  */
@@ -24,6 +38,7 @@ static unsigned long clock_adhtable_set(struct adhtable *ht, const char *key,
  * returned to measure the operation's performance.
  */
 static unsigned long clock_adhtable_get(struct adhtable *ht, const char *key);
+
 /*
  * dhtable_set() but clock_t will be returned.
  */
@@ -33,14 +48,16 @@ static unsigned long clock_dhtable_set(struct dhtable *ht, const char *key,
  * dhtable_get(), but the result is checked and clock_t is returned.
  */
 static unsigned long clock_dhtable_get(struct dhtable *ht, const char *key);
+
 /*
- * Performs benchmarking adhtable
+ * Measures the performance of list_set()
  */
-static void benchmark_adhtable(FILE *fdata, FILE *fset, FILE *fget);
+static unsigned long clock_list_set(struct list *kvlist, const char *key,
+				    const char *value);
 /*
- * Perforams benchmarking dhtable
+ * Sees how list_get() sucks ass.
  */
-static void benchmark_dhtable(FILE *fdata, FILE *fset, FILE *fget);
+static unsigned long clock_list_get(struct list *kvlist, const char *key);
 
 /******************************************************************************/
 
@@ -53,6 +70,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Supported data structures:\n");
 		fprintf(stderr, "\t* adhtable\n");
 		fprintf(stderr, "\t* dhtable\n");
+		fprintf(stderr, "\t* list\n");
 
 		return 1;
 	}
@@ -70,6 +88,8 @@ int main(int argc, char **argv)
 		benchmark_adhtable(fdata, fset, fget);
 	else if (0 == strcmp(argv[1], "dhtable"))
 		benchmark_dhtable(fdata, fset, fget);
+	else if (0 == strcmp(argv[1], "list"))
+		benchmark_list(fdata, fset, fget);
 	else
 		puts("Unsupported data structure");
 
@@ -120,6 +140,24 @@ static void benchmark_dhtable(FILE *fdata, FILE *fset, FILE *fget)
 	dhtable_free(ht);
 }
 
+static void benchmark_list(FILE *fdata, FILE *fset, FILE *fget)
+{
+	char buffer[256];
+	struct list *kvlist;
+
+	kvlist = list_new();
+
+	while (fscanf(fdata, "%s", buffer) != EOF)
+		fprintf(fset, "%lu\n", clock_list_set(kvlist, buffer, buffer));
+
+	rewind(fdata);
+
+	while (fscanf(fdata, "%s", buffer) != EOF)
+		fprintf(fget, "%lu\n", clock_list_get(kvlist, buffer));
+
+	list_string_entry_free(kvlist);
+}
+
 static unsigned long clock_adhtable_get(struct adhtable *ht, const char *key)
 {
 	clock_t start, stop;
@@ -166,4 +204,28 @@ static unsigned long clock_dhtable_set(struct dhtable *ht, const char *key,
 	dhtable_set(ht, key, value);
 
 	return (unsigned long) clock() - start;
+}
+
+static unsigned long clock_list_set(struct list *kvlist, const char *key,
+				    const char *value)
+{
+	clock_t start = clock();
+	list_add(kvlist, string_entry_new(key, value));
+
+	return (unsigned long) clock() - start;
+}
+
+static unsigned long clock_list_get(struct list *kvlist, const char *key)
+{
+	clock_t start, stop;
+	struct string *value;
+
+	start = clock();
+	value = entry_get_value(list_string_entry_find(kvlist, key));
+	stop = clock();
+
+	assert(value);
+	assert(string_equals(value, key));
+
+	return (unsigned long) (stop - start);
 }
